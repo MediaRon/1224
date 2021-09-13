f6f148263042df88f9400d690b41e92ac7f70060
Ronald Huereca <ronalfy@gmail.com>
2021-09-13 13:09:00 -0500

    Adding SVG logo.

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import Logo from './components/Logo/index';
+import './App.css';
+
+const Main = () => {
+  return (
+    <header id="logo">
+      <Logo width="300px" height="auto;" />
+    </header>
+  );
+};
+
+
+export default Main;
