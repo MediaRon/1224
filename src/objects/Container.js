ec6fab55ff712161966098951b07c5e3e6d5b5c7
Ronald Huereca <ronalfy@gmail.com>
2021-09-18 14:05:38 -0500

    Adding a11y to SVG.

--- src/objects/Container.js
+++ src/objects/Container.js
@@ -4,7 +4,7 @@ const AppContainer = styled.div`
 	width: 100%;
 	height: 100vh;
 	background-color: #b0c5e2;
-	background-image: url( hour-glass-sunset-adobe-stock_72239492.jpg );
+	background-image: url( ../images/hour-glass-sunset-adobe-stock_72239492.jpg );
 	background-size: cover;
 	background-position: center center;
 `;
--- src/objects/Header.js
+++ src/objects/Header.js
@@ -2,7 +2,7 @@ import Logo from '../components/Logo';
 import styled from 'styled-components';
 
 const AppHeader = styled.header`
-	padding-top: 1.2em;
+	padding-top: 4rem;
 `;
 
 const Header = () => {
