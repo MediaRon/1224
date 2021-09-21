60f63f8eed1624a88eec8e3c57a0768bc851df3f
Ronald Huereca <ronalfy@gmail.com>
2021-09-21 10:56:19 -0500

    Adding progress meter to app.

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
@@ -7,44 +8,55 @@ const ProgressBarWrapper = styled.div`
 	width: 100%;
 	background: #fff;
 	border-radius: 50px;
-	--duration: 7;
 `;
-
-// From: https://css-tricks.com/timer-bars-in-css-with-custom-properties/
-const roundtime = keyframes`
-		to {
-			transform: scaleX(0);
-		}
-	`;
 
 const ProgressBarInner = styled.div`
 	height: 5px;
 	width: 100%;
-	background: linear-gradient( to bottom, red, #900 );
+	background: linear-gradient( to bottom, #46bf76, #46bf76 );
 	transition: background-color 0.02s ease-in-out;
 	transform-origin: left center;
+	border-radius: 5px;
 `;
 
+const SAFE_THRESHOLD = '#46bf76';
+const WARNING_THRESHOLD = '#bfbe46';
+const DANGER_THRESHOLD = '#bf5a46';
+
 const ProgressBar = ( props ) => {
-	const { percentage, timerCompleted } = props;
+	const { percentage } = props;
 
 	const totalTime = 7000; // /in milliseconds.
 	const warningThreshold = 4500; // in milliseconds.
 	const alertThreshold = 2000; // in milliseconds.
 
+	const [ backgroundColor, setBackgroundColor ] = useState( SAFE_THRESHOLD );
+
+	useEffect( () => {
+		if ( percentage > 25 && percentage < 65 ) {
+			setBackgroundColor( WARNING_THRESHOLD );
+		}
+		if ( percentage < 25 ) {
+			setBackgroundColor( DANGER_THRESHOLD );
+		}
+	}, [ percentage ] );
+
 	return (
 		<ProgressBarWrapper>
-			<ProgressBarInner style={ { width: percentage + '%' } } />
+			<ProgressBarInner
+				style={ {
+					width: percentage + '%',
+					background: backgroundColor,
+				} }
+			/>
 		</ProgressBarWrapper>
 	);
 };
 
 ProgressBar.propTypes = {
 	percentage: PropTypes.number.isRequired,
-	timerCompleted: PropTypes.any,
 };
 ProgressBar.defaultProps = {
 	percentage: 100,
-	timerCompleted: () => {},
 };
 export default ProgressBar;
