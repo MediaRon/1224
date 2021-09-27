a270e70bf0eda3d7d3d382933aa453273f6db446
Ronald Huereca <ronalfy@gmail.com>
2021-09-27 14:44:17 -0500

    Adding power animals.

---
+++ src/components/PowerAnimals/index.js
@@ -0,0 +1,9 @@
+export { default as Bear } from './bear';
+export { default as Elephant } from './elephant';
+export { default as Fox } from './fox';
+export { default as Giraffe } from './giraffe';
+export { default as Hippo } from './hippo';
+export { default as Lion } from './lion';
+export { default as Sloth } from './sloth';
+export { default as Tiger } from './tiger';
+export { default as Zebra } from './zebra';
