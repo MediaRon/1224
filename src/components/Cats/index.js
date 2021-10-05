0e4bd6628ca6395ea4839a45530c125d37f287dc
Ronald Huereca <ronalfy@gmail.com>
2021-10-05 18:40:20 -0500

    Adding cats!

---
+++ src/components/Cats/index.js
@@ -0,0 +1,7 @@
+export { default as Burger } from './Burger';
+export { default as Coffee } from './Coffee';
+export { default as Cone } from './Cone';
+export { default as Cupcake } from './Cupcake';
+export { default as Donut } from './Donut';
+export { default as Fries } from './Fries';
+export { default as Shake } from './Shake';
